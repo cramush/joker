3fd0d92b0472aaf7aa38fb404e2874f1559f306a
cramush <cramush@gmail.com>
2021-08-05 22:16:20 +0300

    code refactor

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,7 @@
 import yaml
 
-secrets = yaml.load(open('.secrets.yaml'), Loader=yaml.Loader)
+DEFAULT_SECRET_PATH = "/etc/joker_bot/secrets.yaml"
+secrets = yaml.load(open(DEFAULT_SECRET_PATH), Loader=yaml.Loader)
 
 db_host = secrets["db_host"]
 db_name = secrets["db_name"]
