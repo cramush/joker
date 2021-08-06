a121c02f156c3d5a8be7fc53f8091a41f8b6c23a
cramush <cramush@gmail.com>
2021-08-06 13:48:30 +0300

    add sslify

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_sslify import SSLify
 from bson.objectid import ObjectId
 from datetime import datetime
 import pymongo
@@ -7,6 +8,7 @@ from waitress import serve
 from loguru import logger
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
 client = pymongo.MongoClient(f"mongodb://{db_host}/{db_name}?authSource=admin")
 db = client["joker_database"]
